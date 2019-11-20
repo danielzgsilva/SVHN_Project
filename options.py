2c3977d0d2e4581cd9ec301116ab26238bb8924a
Daniel Silva <danielzgsilva@knights.ucf.edu>
2019-11-19 19:07:17 -0500

    Options edits

--- options.py
+++ options.py
@@ -12,7 +12,7 @@ class SVHN_Options:
         self.parser.add_argument("--data_path",
                                  type=str,
                                  help="path to the training data",
-                                 default=os.path.join(file_dir, "kitti_data"))
+                                 default=os.path.join(file_dir, 'SVHN_dataset'))
         self.parser.add_argument("--model_path",
                                  type=str,
                                  help="directory to save model weights in",
